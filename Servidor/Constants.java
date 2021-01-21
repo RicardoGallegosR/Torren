a680bd7b25d24e2e405166b9f42906bc4783ff16
RicardoGallegosR <74339822+RicardoGallegosR@users.noreply.github.com>
2021-01-20 21:39:43 -0600

    Update Constants.java

--- Servidor/Constants.java
+++ Servidor/Constants.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Merino
- */
-
 public interface Constants {
 	
 	final static int PEER_TIMEOUT = 5000; 		// 5 segundos para la comunicacion constante del peer y su verificacion
