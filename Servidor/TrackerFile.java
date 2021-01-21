18b58622e290aeb8bd97aec63cd3c85126489c8e
RicardoGallegosR <74339822+RicardoGallegosR@users.noreply.github.com>
2021-01-20 21:40:04 -0600

    Update TrackerFile.java

--- Servidor/TrackerFile.java
+++ Servidor/TrackerFile.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Merino
- */
-
 public class TrackerFile {
 	private String name;	// nombre del archivo
 	private long size;		// tamaño
